f98f43994848012f3fc1ff3089215c08c6e77fbe
nono-yuzu <nonnonpipipi@icloud.com>
2023-03-10 18:46:47 +0900

    first commit

--- DiaryStudy/ContentView.swift
+++ DiaryStudy/ContentView.swift
@@ -6,16 +6,35 @@
 //
 
 import SwiftUI
+import FSCalendar
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            CalendarTestView()
+                .frame(width: 500, height: 500)
+            Spacer()
+            Text("hhjog")
+            
+            
+            
         }
         .padding()
+    }
+    
+    func calendar(_ calendar: FSCalendar, didset date: Date, at monthPosition: FSCalendarMonthPosition) {
+        
+        let tmpCalendar = Calendar(identifier: .gregorian)
+        
+        let month = tmpCalendar.component(.month, from: date)
+        
+        let day = tmpCalendar.component(.day, from: date)
+        
+        let m = String(format: "%02d", month)
+        let d = String(format: "%02d", day)
+        
+        var testText = "\(m)/\(d)"
+        
     }
 }
 
@@ -23,4 +42,19 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct CalendarTestView: UIViewRepresentable {
+    func makeUIView(context: Context) -> UIView {
+        typealias UIViewType = FSCalendar
+        let fscalendar = FSCalendar()
+        
+        return fscalendar
+    }
+    
+    func updateUIView(_ uiView: UIView, context: Context) {
+        
+    }
+    
+    
 }
